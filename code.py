1e59004adb87c6385882f262ebd188000340aa01
hkedia321 <harshit.kedia2015@vit.ac.in>
2017-09-26 05:13:56 +0530

    basic image transform done

--- code.py
+++ code.py
@@ -2,23 +2,47 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 #rnjn
-print "hello world"
-img = cv2.imread('img3.jpg',0)#load in greyscale mode
+print "Starting"
+img = cv2.imread('rice.png',0)#load in greyscale mode
 
 #convert into binary
-ret,thresh1 = cv2.threshold(img,160,255,cv2.THRESH_BINARY)
+ret,binary = cv2.threshold(img,160,255,cv2.THRESH_BINARY)# 160 - threshold, 255 - value to assign, THRESH_BINARY_INV - Inverse binary
 
 #averaging filter
-kernel = np.ones((3,3),np.float32)/9
-dst = cv2.filter2D(thresh1,-1,kernel)
+kernel = np.ones((5,5),np.float32)/9
+dst = cv2.filter2D(binary,-1,kernel)# -1 : depth of the destination image
 
 
+kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 
+#erosion
+erosion = cv2.erode(dst,kernel2,iterations = 1)
 
-plt.subplot(1,3,1),plt.imshow(img,'gray')
+#dilation 
+dilation = cv2.dilate(erosion,kernel2,iterations = 1)
+
+#edge detection
+edges = cv2.Canny(dilation,100,200)
+
+#plot the images
+imgs_row=2
+imgs_col=3
+plt.subplot(imgs_row,imgs_col,1),plt.imshow(img,'gray')
 plt.title("Original image")
-plt.subplot(1,3,2),plt.imshow(thresh1,'gray')
+
+plt.subplot(imgs_row,imgs_col,2),plt.imshow(binary,'gray')
 plt.title("Binary image")
-plt.subplot(1,3,3),plt.imshow(dst,'gray')
+
+plt.subplot(imgs_row,imgs_col,3),plt.imshow(dst,'gray')
 plt.title("Filtered image")
+
+plt.subplot(imgs_row,imgs_col,4),plt.imshow(erosion,'gray')
+plt.title("Eroded image")
+
+plt.subplot(imgs_row,imgs_col,5),plt.imshow(dilation,'gray')
+plt.title("Dialated image")
+
+plt.subplot(imgs_row,imgs_col,6),plt.imshow(edges,'gray')
+plt.title("Edge detect")
+
 plt.show()
