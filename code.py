822891f4206775bd397bb426df754e979ea88422
hkedia321 <harshit.kedia2015@vit.ac.in>
2017-11-06 21:20:09 +0530

    Calculate aspect ratio

--- code.py
+++ code.py
@@ -1,6 +1,20 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+
+def get_classificaton(ratio):
+	ratio =round(ratio,1)
+	toret=""
+	if(ratio>=3):
+		toret="Slender"
+	elif(ratio>=2.1 and ratio<3):
+		toret="Medium"
+	elif(ratio>=1.1 and ratio<2.1):
+		toret="Bold"
+	elif(ratio<=1):
+		toret="Round"
+	toret="("+toret+")"
+	return toret
 #rnjn
 print "Starting"
 img = cv2.imread('rice.png',0)#load in greyscale mode
@@ -24,6 +38,19 @@ dilation = cv2.dilate(erosion,kernel2,iterations = 1)
 #edge detection
 edges = cv2.Canny(dilation,100,200)
 
+### Size detection
+_,contours,hierarchy = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+print "No. of rice grains=",len(contours)
+total_ar=0
+for cnt in contours:
+	x,y,w,h = cv2.boundingRect(cnt)
+	aspect_ratio = float(w)/h
+	if(aspect_ratio<1):
+		aspect_ratio=1/aspect_ratio
+	print round(aspect_ratio,2),get_classificaton(aspect_ratio)
+	total_ar+=aspect_ratio
+avg_ar=total_ar/len(contours)
+print "Average Aspect Ratio=",round(avg_ar,2),get_classificaton(avg_ar)
 #plot the images
 imgs_row=2
 imgs_col=3
@@ -46,3 +73,4 @@ plt.subplot(imgs_row,imgs_col,6),plt.imshow(edges,'gray')
 plt.title("Edge detect")
 
 plt.show()
+
